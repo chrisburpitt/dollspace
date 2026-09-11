e9fe0d5fb202bf04d8594781f17d142cd5a0a0f0
Chris Burpitt <chris.burpitt@gmail.com>
2026-09-12 09:32:42 +1000

    profile badge update

--- src/app/[username]/ProfileClient.tsx
+++ src/app/[username]/ProfileClient.tsx
@@ -78,7 +78,7 @@ export default function ProfileClient({
         {/* CENTER COLUMN: Interactive Switch Feed Renders */}
         <main className="lg:col-span-6 space-y-6">
           <div className="bg-white p-8 rounded-2xl border border-gray-200 shadow-sm pt-14 relative mt-12 sm:mt-16">
-            <div className="absolute -top-14 left-6 sm:left-8 shadow-md overflow-hidden w-28 h-28 flex items-center justify-center shrink-0 select-none z-20">
+            <div className="absolute -top-14 left-6 sm:left-8 rounded-full bg-white shadow-md overflow-hidden w-28 h-28 flex items-center justify-center shrink-0 select-none z-20">
               <AvatarUpload user={user} />
             </div>
 
