559d817c15d1784acc796a6fa1b121d238f4224a
Chris Burpitt <chris.burpitt@gmail.com>
2026-09-17 13:13:18 +1000

    Minor changes to home page

--- src/components/PlatformMetricsCard.tsx
+++ src/components/PlatformMetricsCard.tsx
@@ -28,7 +28,8 @@ export default function PlatformMetricsCard({ metrics }: PlatformMetricsCardProp
             <span className="relative flex h-2 w-2">
               <span className="animate-ping absolute inline-flex h-full w-full rounded-full bg-green-400 opacity-75"></span>
               <span className="relative inline-flex rounded-full h-2 w-2 bg-green-500"></span>
-            Dolls online right now:</span>
+            </span>
+            <span>Dolls online right now:</span>
           </span>
           <span className="text-gray-900 font-black bg-gray-100 px-2 py-0.5 rounded-lg text-[11px]">
             {onlineCount}
