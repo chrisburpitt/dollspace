1ba3244b82b02e223cd7f3a549720049d6f9b1e1
Chris Burpitt <chris.burpitt@gmail.com>
2026-09-17 17:12:45 +1000

    Mobile feed fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -42,7 +42,7 @@ export default async function RootLayout({ children }: { children: React.ReactNo
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
-      <body className="min-h-full flex flex-col">
+      <body className="antialiased overflow-x-hidden w-full max-w-full bg-gray-50">
         {children}
       </body>
     </html>
