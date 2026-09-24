a6cfb207271c234f2abfd7f7870420fb572c66ae
Chris Burpitt <chris.burpitt@gmail.com>
2026-09-24 21:26:41 +1000

    dsbannerii

--- src/components/StaticFeedBanner.tsx
+++ src/components/StaticFeedBanner.tsx
@@ -6,7 +6,7 @@ export default function StaticFeedBanner() {
     <div className="w-full relative overflow-hidden select-none animate-fade-in">
       <div className="w-full h-48 sm:h-64 bg-gray-100 relative">
         <img 
-          src="https://aw7zk78mqn.ufs.sh/f/T14G2VLSOknF6NbZp9GS0RN14GTHXQyMdlnwW3peLFsVYAuq" // Your hardcoded picture URL path
+          src="https://aw7zk78mqn.ufs.sh/f/T14G2VLSOknFtwFu7ACIuodSx1K97TXFWe4pMc36CUN5vZmb" // Your hardcoded picture URL path
           alt="" 
           className="w-full h-full object-cover select-none"
         />
