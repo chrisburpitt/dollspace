7a501df4938b555baef9c3d6c3f0a001f6d4a2b9
Chris Burpitt <chris.burpitt@gmail.com>
2026-09-24 12:24:00 +1000

    DARK rebuild - install Themes v1.4

--- src/components/ThemeProvider.tsx
+++ src/components/ThemeProvider.tsx
@@ -4,5 +4,17 @@ import * as React from "react";
 import { ThemeProvider as NextThemesProvider } from "next-themes";
 
 export function ThemeProvider({ children, ...props }: React.ComponentProps<typeof NextThemesProvider>) {
-  return <NextThemesProvider {...props}>{children}</NextThemesProvider>;
+  return (
+    // 🚀 THE THEME FORCE MULTIPLIER:
+    // Explicitly mapping attribute="class" and defaultTheme="light" forces next-themes 
+    // to write "class='dark'" onto the HTML element, unblocking Tailwind instantly!
+    <NextThemesProvider 
+      attribute="class" 
+      defaultTheme="light" 
+      enableSystem={false}
+      {...props}
+    >
+      {children}
+    </NextThemesProvider>
+  );
 }
